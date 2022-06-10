9bd0dab62912f7f2bb53b254f32a30c8ecf0dc72
Claire Azagra <claire.azagra@gmail.com>
2022-06-11 00:10:12 +0800

    app done

---
+++ Contact Tracing/Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace Contact_Tracing
+{
+    partial class IdentityForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.identityverificationform = new System.Windows.Forms.Label();
+            this.lblFullname = new System.Windows.Forms.Label();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtFullname = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblEmailaddress = new System.Windows.Forms.Label();
+            this.txtEmailaddress = new System.Windows.Forms.TextBox();
+            this.txtcontactnum = new System.Windows.Forms.TextBox();
+            this.Btnsubmit = new System.Windows.Forms.Button();
+            this.lblcontactnum = new System.Windows.Forms.Label();
+            this.txtCompleteaddress = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.lblCivilstatus = new System.Windows.Forms.Label();
+            this.txtCivilstatus = new System.Windows.Forms.TextBox();
+            this.lblGender = new System.Windows.Forms.Label();
+            this.txtGender = new System.Windows.Forms.TextBox();
+            this.lblReligion = new System.Windows.Forms.Label();
+            this.txtReligion = new System.Windows.Forms.TextBox();
+            this.lblinstruction = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // identityverificationform
+            // 
+            this.identityverificationform.AutoSize = true;
+            this.identityverificationform.Font = new System.Drawing.Font("MS Reference Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.identityverificationform.Location = new System.Drawing.Point(72, 27);
+            this.identityverificationform.Name = "identityverificationform";
+            this.identityverificationform.Size = new System.Drawing.Size(282, 24);
+            this.identityverificationform.TabIndex = 0;
+            this.identityverificationform.Text = "Identity Verification Form";
+            // 
+            // lblFullname
+            // 
+            this.lblFullname.AutoSize = true;
+            this.lblFullname.Location = new System.Drawing.Point(24, 134);
+            this.lblFullname.Name = "lblFullname";
+            this.lblFullname.Size = new System.Drawing.Size(57, 13);
+            this.lblFullname.TabIndex = 1;
+            this.lblFullname.Text = "Full Name:";
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Location = new System.Drawing.Point(24, 229);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(95, 13);
+            this.lblAddress.TabIndex = 2;
+            this.lblAddress.Text = "Complete Address:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(144, 169);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 3;
+            // 
+            // txtFullname
+            // 
+            this.txtFullname.Location = new System.Drawing.Point(133, 127);
+            this.txtFullname.Name = "txtFullname";
+            this.txtFullname.Size = new System.Drawing.Size(248, 20);
+            this.txtFullname.TabIndex = 4;
+            this.txtFullname.WordWrap = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(262, 169);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(24, 254);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 8;
+            // 
+            // lblEmailaddress
+            // 
+            this.lblEmailaddress.AutoSize = true;
+            this.lblEmailaddress.Location = new System.Drawing.Point(24, 293);
+            this.lblEmailaddress.Name = "lblEmailaddress";
+            this.lblEmailaddress.Size = new System.Drawing.Size(76, 13);
+            this.lblEmailaddress.TabIndex = 10;
+            this.lblEmailaddress.Text = "Email Address:";
+            // 
+            // txtEmailaddress
+            // 
+            this.txtEmailaddress.Location = new System.Drawing.Point(133, 286);
+            this.txtEmailaddress.Name = "txtEmailaddress";
+            this.txtEmailaddress.Size = new System.Drawing.Size(248, 20);
+            this.txtEmailaddress.TabIndex = 11;
+            // 
+            // txtcontactnum
+            // 
+            this.txtcontactnum.Location = new System.Drawing.Point(133, 332);
+            this.txtcontactnum.Name = "txtcontactnum";
+            this.txtcontactnum.Size = new System.Drawing.Size(149, 20);
+            this.txtcontactnum.TabIndex = 12;
+            // 
+            // Btnsubmit
+            // 
+            this.Btnsubmit.BackColor = System.Drawing.Color.RosyBrown;
+            this.Btnsubmit.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this.Btnsubmit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightCoral;
+            this.Btnsubmit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btnsubmit.Location = new System.Drawing.Point(300, 393);
+            this.Btnsubmit.Name = "Btnsubmit";
+            this.Btnsubmit.Size = new System.Drawing.Size(79, 24);
+            this.Btnsubmit.TabIndex = 17;
+            this.Btnsubmit.Text = "Submit";
+            this.Btnsubmit.UseVisualStyleBackColor = false;
+            this.Btnsubmit.Click += new System.EventHandler(this.Btnsubmit_Click);
+            // 
+            // lblcontactnum
+            // 
+            this.lblcontactnum.AutoSize = true;
+            this.lblcontactnum.Location = new System.Drawing.Point(24, 335);
+            this.lblcontactnum.Name = "lblcontactnum";
+            this.lblcontactnum.Size = new System.Drawing.Size(87, 13);
+            this.lblcontactnum.TabIndex = 18;
+            this.lblcontactnum.Text = "Contact Number:";
+            // 
+            // txtCompleteaddress
+            // 
+            this.txtCompleteaddress.Location = new System.Drawing.Point(133, 229);
+            this.txtCompleteaddress.Name = "txtCompleteaddress";
+            this.txtCompleteaddress.Size = new System.Drawing.Size(248, 38);
+            this.txtCompleteaddress.TabIndex = 19;
+            this.txtCompleteaddress.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(262, 169);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Age:";
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(330, 164);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(51, 20);
+            this.txtAge.TabIndex = 21;
+            // 
+            // lblCivilstatus
+            // 
+            this.lblCivilstatus.AutoSize = true;
+            this.lblCivilstatus.Location = new System.Drawing.Point(262, 194);
+            this.lblCivilstatus.Name = "lblCivilstatus";
+            this.lblCivilstatus.Size = new System.Drawing.Size(62, 13);
+            this.lblCivilstatus.TabIndex = 22;
+            this.lblCivilstatus.Text = "Civil Status:";
+            // 
+            // txtCivilstatus
+            // 
+            this.txtCivilstatus.Location = new System.Drawing.Point(330, 190);
+            this.txtCivilstatus.Name = "txtCivilstatus";
+            this.txtCivilstatus.Size = new System.Drawing.Size(51, 20);
+            this.txtCivilstatus.TabIndex = 23;
+            // 
+            // lblGender
+            // 
+            this.lblGender.AutoSize = true;
+            this.lblGender.Location = new System.Drawing.Point(130, 169);
+            this.lblGender.Name = "lblGender";
+            this.lblGender.Size = new System.Drawing.Size(45, 13);
+            this.lblGender.TabIndex = 24;
+            this.lblGender.Text = "Gender:";
+            // 
+            // txtGender
+            // 
+            this.txtGender.Location = new System.Drawing.Point(184, 165);
+            this.txtGender.Name = "txtGender";
+            this.txtGender.Size = new System.Drawing.Size(60, 20);
+            this.txtGender.TabIndex = 25;
+            // 
+            // lblReligion
+            // 
+            this.lblReligion.AutoSize = true;
+            this.lblReligion.Location = new System.Drawing.Point(130, 194);
+            this.lblReligion.Name = "lblReligion";
+            this.lblReligion.Size = new System.Drawing.Size(48, 13);
+            this.lblReligion.TabIndex = 26;
+            this.lblReligion.Text = "Religion:";
+            // 
+            // txtReligion
+            // 
+            this.txtReligion.Location = new System.Drawing.Point(184, 191);
+            this.txtReligion.Name = "txtReligion";
+            this.txtReligion.Size = new System.Drawing.Size(60, 20);
+            this.txtReligion.TabIndex = 27;
+            // 
+            // lblinstruction
+            // 
+            this.lblinstruction.Location = new System.Drawing.Point(25, 66);
+            this.lblinstruction.Name = "lblinstruction";
+            this.lblinstruction.Size = new System.Drawing.Size(354, 33);
+            this.lblinstruction.TabIndex = 28;
+            this.lblinstruction.Text = "Please fill the form accurately, as information provided will be used in identity" +
+    " card and also in your personal form.";
+            // 
+            // IdentityForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Thistle;
+            this.ClientSize = new System.Drawing.Size(406, 447);
+            this.Controls.Add(this.lblinstruction);
+            this.Controls.Add(this.txtReligion);
+            this.Controls.Add(this.lblReligion);
+            this.Controls.Add(this.txtGender);
+            this.Controls.Add(this.lblGender);
+            this.Controls.Add(this.txtCivilstatus);
+            this.Controls.Add(this.lblCivilstatus);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCompleteaddress);
+            this.Controls.Add(this.lblcontactnum);
+            this.Controls.Add(this.Btnsubmit);
+            this.Controls.Add(this.txtcontactnum);
+            this.Controls.Add(this.txtEmailaddress);
+            this.Controls.Add(this.lblEmailaddress);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtFullname);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblAddress);
+            this.Controls.Add(this.lblFullname);
+            this.Controls.Add(this.identityverificationform);
+            this.Name = "IdentityForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Application Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label identityverificationform;
+        private System.Windows.Forms.Label lblFullname;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtFullname;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblEmailaddress;
+        private System.Windows.Forms.TextBox txtEmailaddress;
+        private System.Windows.Forms.TextBox txtcontactnum;
+        private System.Windows.Forms.Button Btnsubmit;
+        private System.Windows.Forms.Label lblcontactnum;
+        private System.Windows.Forms.RichTextBox txtCompleteaddress;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.Label lblCivilstatus;
+        private System.Windows.Forms.TextBox txtCivilstatus;
+        private System.Windows.Forms.Label lblGender;
+        private System.Windows.Forms.TextBox txtGender;
+        private System.Windows.Forms.Label lblReligion;
+        private System.Windows.Forms.TextBox txtReligion;
+        private System.Windows.Forms.Label lblinstruction;
+    }
+}
+
---
+++ Contact Tracing/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Contact_Tracing
+{
+    public partial class IdentityForm : Form
+    {
+        public IdentityForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btnsubmit_Click(object sender, EventArgs e)
+        {
+            StreamWriter file = new StreamWriter(@"C:\Users\dell\source\repos\Contact Tracing\identityform.txt", true);
+            file.WriteLine("Full Name: " + txtFullname.Text);
+            file.WriteLine("Gender: " + txtGender.Text);
+            file.WriteLine("Age: " + txtAge.Text);
+            file.WriteLine("Religion: " + txtReligion.Text);
+            file.WriteLine("Cilvil Status: " + txtCivilstatus.Text);
+            file.WriteLine("Complete Address: " + txtCompleteaddress.Text);
+            file.WriteLine("Email Address: " + txtEmailaddress.Text);
+            file.WriteLine("Contact Number: " + txtcontactnum.Text);
+            file.Close();
+
+            StreamReader reader = new StreamReader(@"C:\Users\dell\source\repos\Contact Tracing\identityform.txt");
+            String all = reader.ReadToEnd();
+            MessageBox.Show(all, "Identity Form");
+
+           
+                
+        }
+        
+    }
+}
